49b5e7506572d27a486a2ebf0c88973bec402bb9
marcvdg <43010761+marcvdg@users.noreply.github.com>
2020-02-03 14:49:43 +0100

    credit completion

--- credit.c
+++ credit.c
@@ -1,10 +1,87 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int geteven(int p, int cc)
+int main(void)
 {
-    return (cc / p) % p;
+    long cc;
+    do
+    {
+        cc = get_long("Number: ");
+    }
+    while (5999999999999999 < cc && cc < 39999999999999);
+    long n = cc;
+    int p;
+    int unevens = 0;
+    int evens = 0;
+    int count = 0;
+    int thirteen, fourteen, fifteen, sixteen;
+    for (int i = 0; i < 16; i++)
+    {
+        p = n%10;
+        //counter
+        if (n > 0)
+        {
+            count++;
+            switch (count)
+            {   
+                case 13: thirteen = p;
+                    break;
+                case 14: fourteen = p;
+                    break;
+                case 15: fifteen = p;
+                    break;
+                case 16: sixteen = p;
+                    break;
+            }
+        }
+   
+        //check if uneven or even
+        if ( i % 2 == 0 )
+        {
+            //uneven numbers from right
+            unevens += p;
+        }
+        else
+        {
+            //evens
+            int q = p * 2;
+            for (int j = 0; j < 2; j++)
+            {
+                int r = q % 10;
+                evens += r;
+                q /= 10;
+            }
+        }      
+        n /= 10;
+    }
+    int total = evens + unevens;
+    if (total%10 == 0)
+    {
+        if ((count == 13 && thirteen == 4) || (count == 16 && sixteen == 4))
+        {
+            printf("VISA\n");
+        }
+        else if (count == 16 && sixteen == 5 && ( 6 > fifteen > 0))
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (count == 15 && fifteen == 3 && (fourteen == 4 || fourteen ==7))
+        {
+            printf("AMEX\n");
+        }
+        else 
+        {
+            printf("INVALID\n");
+        }
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
+
+#include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -13,12 +90,75 @@ int main(void)
     {
         cc = get_long("Number: ");
     }
-    while (cc < 10);
-
-    int second = geteven(10, cc);
-    int fourth = getsec(1000, cc);
-    printf("%li\n", cc);
-    printf("%i\n", second);
-    printf("%i\n", fourth);
+    while (5999999999999999 < cc && cc < 39999999999999);
+    long n = cc;
+    int p;
+    int unevens = 0;
+    int evens = 0;
+    int count = 0;
+    int thirteen, fourteen, fifteen, sixteen;
+    for (int i = 0; i < 16; i++)
+    {
+        p = n%10;
+        //counter
+        if (n > 0)
+        {
+            count++;
+            switch (count)
+            {   
+                case 13: thirteen = p;
+                    break;
+                case 14: fourteen = p;
+                    break;
+                case 15: fifteen = p;
+                    break;
+                case 16: sixteen = p;
+                    break;
+            }
+        }
+   
+        //check if uneven or even
+        if ( i % 2 == 0 )
+        {
+            //uneven numbers from right
+            unevens += p;
+        }
+        else
+        {
+            //evens
+            int q = p * 2;
+            for (int j = 0; j < 2; j++)
+            {
+                int r = q % 10;
+                evens += r;
+                q /= 10;
+            }
+        }      
+        n /= 10;
+    }
+    int total = evens + unevens;
+    if (total%10 == 0)
+    {
+        if ((count == 13 && thirteen == 4) || (count == 16 && sixteen == 4))
+        {
+            printf("VISA\n");
+        }
+        else if (count == 16 && sixteen == 5 && ( 6 > fifteen > 0))
+        {
+            printf("MASTERCARD\n");
+        }
+        else if (count == 15 && fifteen == 3 && (fourteen == 4 || fourteen ==7))
+        {
+            printf("AMEX\n");
+        }
+        else 
+        {
+            printf("INVALID\n");
+        }
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
 }
 
